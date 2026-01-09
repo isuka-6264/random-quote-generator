49e280f3e76d1d0ebcc447b62e79b4943365f316
isuka-6264 <destiny.vml@gmail.com>
2026-01-09 22:44:58 +0300

    Fixing error

--- src/main.py
+++ src/main.py
@@ -1,9 +1,11 @@
 from loader import load_quotes
 from randomizer import get_random_quote
 
+
 def main():
     quotes = load_quotes("quotes/quotes.txt")
     print(get_random_quote(quotes))
+
 
 if __name__ == "__main__":
     main()
